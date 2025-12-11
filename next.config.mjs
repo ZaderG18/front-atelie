789b337d2e8673bc12a7b0dc88de64525e2ba0ea
ZaderG18 <arthurhenriquegr818@gmail.com>
2025-12-11 20:14:51 -0300

    aaaaaaaaaa

--- next.config.mjs
+++ next.config.mjs
@@ -7,17 +7,23 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [
       {
+        protocol: 'https',
+        hostname: 'atelie-aflorar-api.onrender.com', // <--- Adicionamos o Render aqui
+        port: '',
+        pathname: '/storage/**',
+      },
+      {
         protocol: 'http',
         hostname: '127.0.0.1',
         port: '8000',
         pathname: '/storage/**',
       },
       {
-         protocol: 'http',
-         hostname: 'localhost',
-         port: '8000',
-         pathname: '/storage/**',
-       },
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8000',
+        pathname: '/storage/**',
+      },
     ],
   },
 };
