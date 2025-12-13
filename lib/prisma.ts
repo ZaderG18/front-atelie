32e8bd2ce3e35e2d1ca64a8d1b452bbb473bfd95
ZaderG18 <arthurhenriquegr818@gmail.com>
2025-12-13 15:45:15 -0300

    Fix: Implement Prisma Singleton to prevent connection leak

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+// Adiciona o prisma ao escopo global do NodeJS para evitar recriação
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
 
-// Remova o objeto de configuração daqui também
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+declare global {
+  var prismaGlobal: undefined | ReturnType<typeof prismaClientSingleton>
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// Se já existir uma conexão global, usa ela. Se não, cria uma nova.
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+// Em desenvolvimento, salva a conexão na variável global para sobreviver ao hot-reload
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = prisma
+}
